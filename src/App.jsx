baf05f0d3d69b9b1c9bfe4ef3c9052630cd2801e
shivamss454 <shivam.sharma@successive.tech>
2020-03-26 16:33:54 +0530

    fix: updated material ui form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
 // import Textfielddemo from './pages/TextFieldDemo/TextFieldDemo';
 // import { InputDemo } from './pages/InputDemo';
 // import ChildrenDemo from './pages/ChildrenDemo/ChildrenDemo';
-import theme from './theme';
 import Trainee from './pages/Trainee/Trainee';
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Trainee />
-      </ThemeProvider>
-
+      <Trainee />
     </>
   );
 }
