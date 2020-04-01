edcc5e23c47b4cef5ebc5b0d391f5c9b8dde82c1
shivamss454 <shivam.sharma@successive.tech>
2020-04-01 14:48:56 +0530

    fix: remove comments from app.j

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import Textfielddemo from './pages/TextFieldDemo/TextFieldDemo';
-// import { InputDemo } from './pages/InputDemo';
-// import ChildrenDemo from './pages/ChildrenDemo/ChildrenDemo';
 import Trainee from './pages/Trainee/Trainee';
 
 function App() {
