fd332a59b64ff32e9988b38010bba8980eb9e4c0
shivamss454 <shivam.sharma@successive.tech>
2020-04-01 14:52:56 +0530

    fix: remove login from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Trainee from './pages/Trainee/Trainee';
-import Login from './pages/Login/Login';
 
 function App() {
   return (
     <>
       <Trainee />
-      <Login />
     </>
   );
 }
