030da41e13cfdab7afdd53f201e3ec5dba0c2e7a
shivamss454 <shivam.sharma@successive.tech>
2020-05-27 21:21:56 +0530

    fix: change in TraineeDetail

--- src/pages/Trainee/TraineeDetail.jsx
+++ src/pages/Trainee/TraineeDetail.jsx
@@ -3,28 +3,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import { CardActions } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import moment from 'moment';
 import { useParams, useHistory } from 'react-router-dom';
-import Button from '../../components/Button/Button';
+import Button from '@material-ui/core/Button';
 import trainee from './data/trainee';
 import NoMatch from '../NoMatch/NoMatch';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  margin: {
+    margin: theme.spacing(2),
+  },
   detail: {
     display: 'flex',
     flexDirection: 'column',
   },
-  root: {
-    display: 'flex',
-  },
-  media: {
-    width: 121,
-  },
   content: {
     flex: '1 0 auto',
   },
-});
+  cover: {
+    width: 141,
+  },
+}));
+
 
 function getDateFormatted(date) {
   return moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a');
@@ -43,7 +48,7 @@ export default function TraineeDetail() {
     <>
       <Card className={classes.root}>
         <CardMedia
-          className={classes.media}
+          className={classes.cover}
           image="https://image.flaticon.com/icons/svg/21/21104.svg"
           title="usercover"
         />
@@ -55,17 +60,16 @@ export default function TraineeDetail() {
             <Typography variant="subtitle1" color="textsecondary">
               {getDateFormatted(found.createdAt)}
             </Typography>
-            <Typography component="h5" variant="subtitled">
+            <Typography component="h5" variant="subtitle2">
               {found.email}
             </Typography>
           </CardContent>
         </div>
-
       </Card>
       <br />
-      <div align="center">
-        <Button onClick={() => history.goBack()} value="Back" />
-      </div>
+      <CardActions style={{ justifyContent: 'center' }}>
+        <Button variant="contained" size="medium" className={classes.margin} onClick={() => history.push('/trainee')}>Back</Button>
+      </CardActions>
     </>
   );
 }
